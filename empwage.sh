5bc31f9606929c8e9bbf0d16bfb76d56dc896155
nagarajnsg1995 <nagarajaece.rymec@gmail.com>
2022-05-06 11:50:21 +0530

    employe present or not

--- empwage.sh
+++ empwage.sh
@@ -1,3 +1,14 @@
-#!bin/bash
+#!bin/bash -x
 
-echo	"wellcomeO"
+ispresent=1;
+randomcheck=$(( RANDOM%2 ))
+
+if [ $ispresent -eq $randomcheck ]
+then
+echo    "employee is present"
+
+else
+echo    "employee is absent"
+
+fi
+
